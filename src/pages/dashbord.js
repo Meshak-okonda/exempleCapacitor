9ddae66125f4de86816c1b520b72ed4afadda3c3
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-02-26 21:29:00 +0100

    Update dashbord.js

--- src/pages/dashbord.js
+++ src/pages/dashbord.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Box, Container, Grid } from "@mui/material";
 import { Budget } from "../components/dashboard/budget";
+import StatCar from "../components/dashboard/StatCar";
 import { LatestOrders } from "../components/dashboard/latest-orders";
 import { LatestProducts } from "../components/dashboard/latest-products";
 import { Sales } from "../components/dashboard/sales";
@@ -9,50 +10,47 @@ import { TotalCustomers } from "../components/dashboard/total-customers";
 import { TotalProfit } from "../components/dashboard/total-profit";
 import { TrafficByDevice } from "../components/dashboard/traffic-by-device";
 import { DashboardLayout } from "../components/dashboard-layout";
+import { useAppSelector } from "../hooks";
 
-const Dashboard = () => (
-  <>
-    <Head>
-      <title>Dashboard | Material Kit</title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8,
-      }}
-    >
-      <Container maxWidth={false}>
-        <Grid container spacing={3}>
-          <Grid item lg={3} sm={6} xl={3} xs={12}>
-            <Budget />
+const Dashboard = () => {
+  const { vehicles } = useAppSelector((state) => state.globalState);
+
+  return (
+    <>
+      <Head>
+        <title>Dashboard | Material Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <Grid container spacing={3}>
+            <Grid item lg={4} sm={6} xl={4} xs={12}>
+              <Budget />
+            </Grid>
+            <Grid item xl={4} lg={4} sm={6} xs={12}>
+              <TotalCustomers />
+            </Grid>
+            <Grid item xl={4} lg={4} sm={6} xs={12}>
+              <TasksProgress />
+            </Grid>
+            {vehicles.length &&
+              vehicles
+                .filter((veh) => veh.delete != true)
+                .map((vehicle) => (
+                  <Grid item lg={4} md={6} xl={3} xs={12}>
+                    <StatCar key={vehicle.id} vehicle={vehicle} />
+                  </Grid>
+                ))}
           </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TotalCustomers />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TasksProgress />
-          </Grid>
-          <Grid item xl={3} lg={3} sm={6} xs={12}>
-            <TotalProfit sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <Sales />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <TrafficByDevice sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={4} md={6} xl={3} xs={12}>
-            <LatestProducts sx={{ height: "100%" }} />
-          </Grid>
-          <Grid item lg={8} md={12} xl={9} xs={12}>
-            <LatestOrders />
-          </Grid>
-        </Grid>
-      </Container>
-    </Box>
-  </>
-);
+        </Container>
+      </Box>
+    </>
+  );}
 
 Dashboard.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
