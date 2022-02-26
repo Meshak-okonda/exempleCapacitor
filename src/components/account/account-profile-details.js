9ab62e540192d2992d6f89ce2081d3256872f714
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-02-26 21:27:17 +0100

    Update account-profile-details.js

--- src/components/account/account-profile-details.js
+++ src/components/account/account-profile-details.js
@@ -1,26 +1,107 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import {
   Box,
-  Button,
   Card,
   CardContent,
   CardHeader,
   Divider,
   Grid,
-  TextField
 } from '@mui/material';
+import { UPDATE_RESPONSABLE } from "../../graphql/queries";
+import { useForm, Controller } from 'react-hook-form';
+import { GetAges, GetSex, getDate } from "../../utils";
 import { useAppDispatch, useAppSelector } from "../../hooks";
+import { updateResponsableInState } from "../../redux/slice/globalSlice";
+import PopUpMutation from "../custom/PopUpMutation";
+import ToastCustom from "../ToastCustom";
+import PropTypes from "prop-types";
+import Button from "@mui/material/Button";
+import { styled } from "@mui/material/styles";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+import Typography from "@mui/material/Typography";
+import Select from "@mui/material/Select";
+import TextField from "@mui/material/TextField";
+import MenuItem from "@mui/material/MenuItem";
+import InputLabel from "@mui/material/InputLabel";
+import Switch from "@mui/material/Switch";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import ButtonSubmit from "../ButtonSubmit";
 
 
 export const AccountProfileDetails = (props) => {
   const { user } = useAppSelector((state) => state.userConnected);
+  const dispatch = useAppDispatch();
+	const [data, setData] = useState(null);
+  const [modalOn, setModalOn] = useState(false);
+	const [variablesMutation, setVariablesMutation] = useState({});
+	const handleClose = () => setModalON(false);
+	const [toast, setToast] = useState({
+		state: false,
+		message: '',
+		type: '',
+		header: '',
+		delay: null,
+	});
+
+	useEffect(() => {
+		if (data) {
+			const { updateResponsable } = data;
+			dispatch(updateResponsableInState(updateResponsable));
+			localStorage.removeItem('user');
+			localStorage.setItem(
+        "user",
+        JSON.stringify({ ...updateResponsable, date: getDate() })
+      );
+			setTimeout(() => {
+				setModalON(false);
+			}, 1000);
+		}
+	}, [data, dispatch]);
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+		control,
+	} = useForm();
+
+	async function onSubmit(data) {
+    const { token, createdAt, id, date, superAdm, password, ...otherInfo } = user;
+	console.log({ ...otherInfo, ...data });
+    setVariablesMutation({ ...otherInfo, ...data });
+    setModalOn(true);
+  }
+	const recupNumberInString = (string) => {
+		const regex = /\d+/g;
+		const result = string.match(regex);
+		return result[0];
+	};
+
+  const confirmMutation = async (mutation) => {
+		try {
+			await mutation({
+				variables: {
+					updateResponsableId: user.id,
+					responsable: { ...variablesMutation, image: user.image },
+				},
+			});
+		} catch (error) {
+			setToast({
+				state: true,
+				message: JSON.stringify(error.message),
+				type: "danger",
+				header: "Erreur",
+				delay: 3000,
+			});
+		}
+	};
 
   return (
-    <form
-      autoComplete="off"
-      noValidate
-      {...props}
-    >
+    <form onSubmit={handleSubmit(onSubmit)}>
       <Card>
         <CardHeader
           subheader="Mis à jour du profil"
@@ -37,96 +118,288 @@ export const AccountProfileDetails = (props) => {
               md={6}
               xs={12}
             >
-              <TextField
-                fullWidth
-                helperText="Veuillez entrer le nom d'utilisateur Svp"
-                label="Nom d'utilisateur"
-                name="Nom d'utilisateur"
-                required
-                value={user.name}
-                variant="outlined"
-              />
+              <Controller
+					name='name'
+					control={control}
+					rules={{
+						required: true,
+						pattern: /^[a-zA-Z]+$/,
+						maxLength: 20,
+						minLength: 3,
+					}}
+					defaultValue={user.name}
+					render={({ field }) => {
+						return (
+							<>
+							<TextField
+									fullWidth
+									label="Nom *"
+									variant="outlined"
+									id={field.name}
+									error={errors.name}
+									{...field}
+									value={field.value}
+									type="text"
+								/>
+								{errors.name && errors.name.type === 'required' && (
+									<small className='alert-danger p-1'>
+										Le nom est requis !
+									</small>
+								)}
+								{errors.name && errors.name.type === 'maxLength' && (
+									<small className='alert-danger p-1'>
+										La taille minimal n'est pas respecter (20 caractères)
+									</small>
+								)}
+								{errors.name && errors.name.type === 'minLength' && (
+									<small className='alert-danger p-1'>
+										La taille minimal n'est pas respecter (3 caractères)
+									</small>
+								)}
+								{errors.name && errors.name.type === 'pattern' && (
+									<small className='alert-danger p-1'>
+										Le nom ne doit contenir que des lettres
+									</small>
+								)}
+							</>
+						);
+					}}
+				/>
             </Grid>
             <Grid
               item
               md={6}
               xs={12}
             >
-              <TextField
-                fullWidth
-                label="Last name"
-                name="lastName"
-                required
-                value={user.lastName}
-                variant="outlined"
-              />
+              <Controller
+					name='lastName'
+					control={control}
+					rules={{
+						required: true,
+						pattern: /^[a-zA-Z]+$/,
+						maxLength: 20,
+						minLength: 3,
+					}}
+					defaultValue={user.lastName}
+					render={({ field }) => {
+						return (
+							<>
+								<TextField
+									fullWidth
+									label="Post nom *"
+									variant="outlined"
+									id={field.name}
+									error={errors.name}
+									{...field}
+									value={field.value}
+									type="text"
+								/>
+												{errors.lastName && errors.lastName.type === 'required' && (
+					<small className='alert-danger p-1'>
+						Le post nom est requis !
+					</small>
+				)}
+				{errors.lastName && errors.lastName.type === 'maxLength' && (
+					<small className='alert-danger p-1'>
+						La taille minimal n'est pas respecter (20 caractères)
+					</small>
+				)}
+				{errors.lastName && errors.lastName.type === 'minLength' && (
+					<small className='alert-danger p-1'>
+						La taille minimal n'est pas respecter (3 caractères)
+					</small>
+				)}
+				{errors.lastName && errors.lastName.type === 'pattern' && (
+					<small className='alert-danger p-1'>
+						le post nom ne doit contenir que des lettres
+					</small>
+				)}
+							</>
+						);
+					}}
+				/>
             </Grid>
             <Grid
               item
               md={6}
               xs={12}
             >
-              <TextField
-                fullWidth
-                label="Adresse mail"
-                name="email"
-                required
-                value={user.email}
-                variant="outlined"
-              />
+              <Controller
+					name='email'
+					control={control}
+					rules={{
+						maxLength: 50,
+						minLength: 3,
+						pattern:
+							/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+					}}
+					defaultValue={user.email}
+					render={({ field }) => {
+						return (
+							<>
+							<TextField
+								fullWidth
+								label="E - Mail *"
+								variant="outlined"
+								id={field.name}
+								error={errors.name}
+								{...field}
+								value={field.value}
+								type="text"
+							/>
+							{errors.email && errors.email.type === 'maxLength' && (
+								<small className='alert-danger p-1'>
+									La taille minimal n'est pas respecter (20 caractères)
+								</small>
+							)}
+							{errors.email && errors.email.type === 'minLength' && (
+								<small className='alert-danger p-1'>
+									La taille minimal n'est pas respecter (3 caractères)
+								</small>
+							)}
+							{errors.email && errors.email.type === 'pattern' && (
+								<small className='alert-danger p-1'>email invalide</small>
+							)}
+							</>
+						);
+					}}
+				/>
             </Grid>
             <Grid
               item
               md={6}
               xs={12}
             >
-              <TextField
-                fullWidth
-                label="Numero de telephone"
-                name="phone"
-                type="number"
-                value={user.phone}
-                variant="outlined"
-              />
+              <Controller
+					name='phone'
+					control={control}
+					rules={{
+						required: true,
+						maxLength: 50,
+						minLength: 8,
+						pattern: /^[0-9]+$/,
+					}}
+					defaultValue={user.phone}
+					render={({ field }) => {
+						return (
+							<>
+							<TextField
+								fullWidth
+								label="Numero de telephone *"
+								variant="outlined"
+								id={field.name}
+								error={errors.name}
+								{...field}
+								value={field.value}
+								type="text"
+							/>
+							{errors.phone && errors.phone.type === 'required' && (
+								<small className='alert-danger p-1'>
+									le mot de passe est requis !
+								</small>
+							)}
+							{errors.phone && errors.phone.type === 'maxLength' && (
+								<small className='alert-danger p-1'>
+									La taille minimal n'est pas respecter (50 caractères)
+								</small>
+							)}
+							{errors.phone && errors.phone.type === 'minLength' && (
+								<small className='alert-danger p-1'>
+									La taille minimal n'est pas respecter (8 caractères)
+								</small>
+							)}
+							{errors.phone && errors.phone.type === 'pattern' && (
+								<small className='alert-danger p-1'>
+									le format du numero de telephone est invalide (0-9)
+								</small>
+							)}
+							</>
+						);
+					}}
+				/>
             </Grid>
             <Grid
               item
               md={6}
               xs={12}
             >
-              <TextField
-                fullWidth
-                label="Mot de pase"
-                name="password"
-                required
-                value={user.password}
-                variant="outlined"
-              />
+              <Controller
+					name='age'
+					control={control}
+					rules={{
+						required: true,
+					}}
+					defaultValue={user.age}
+					render={({ field }) => {
+						return (
+							<>
+							<InputLabel id="demo-simple-select-standard-label">
+								Age *
+							</InputLabel>
+							<Select
+								labelId="demo-simple-select-label"
+								id="demo-simple-select"
+								value={`${field.value}`}
+								label="Age *"
+								fullWidth
+								onChange={({ target: { value } }) => {
+								field.onChange(parseInt(value));
+								}}
+							>
+								{GetAges().map(({ value, label }) => (
+								<MenuItem value={value}>{label}</MenuItem>
+								))}
+							</Select>
+							{errors.age && (
+								<small className='alert-danger p-1'>L'age est requis !</small>
+							)}
+							</>
+						);
+					}}
+				/>
             </Grid>
             <Grid
               item
               md={6}
               xs={12}
             >
-              <TextField
+              <Controller
+					name='sex'
+					control={control}
+					rules={{
+						required: true,
+					}}
+					defaultValue={user.sex}
+					render={({ field }) => {
+						return (
+							<>
+							<InputLabel id="demo-simple-select-standard-label">
+                Sex *
+              </InputLabel>
+              <Select
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={field.value}
+                label="Sexe du responsable"
+				placeholder={field.value}
                 fullWidth
-                label="Select State"
-                name="state"
-                required
-                select
-                SelectProps={{ native: true }}
-                value={user.name}
-                variant="outlined"
+                onChange={({ target: { value } }) => {
+                  field.onChange(value);
+                }}
               >
-                {/* {states.map((option) => (
-                  <option
-                    key={option.value}
-                    value={option.value}
-                  >
-                    {option.label}
-                  </option>
-                ))} */}
-              </TextField>
+                {GetSex().map(({ value, label }) => (
+                  <MenuItem value={value}>{label}</MenuItem>
+                ))}
+              </Select>
+						  {errors.sex && (
+							<small className='alert-danger p-1'>
+								Le sexe est requis !
+							</small>
+						)}
+						</>
+						);
+					}}
+				/>
             </Grid>
           </Grid>
         </CardContent>
@@ -138,14 +411,27 @@ export const AccountProfileDetails = (props) => {
             p: 2
           }}
         >
-          <Button
-            color="primary"
-            variant="contained"
-          >
-            Save details
-          </Button>
+          <ButtonSubmit autoFocus type="submit" />
         </Box>
       </Card>
-    </form>
+        {modalOn && (
+          <PopUpMutation
+            query={UPDATE_RESPONSABLE}
+            setModalON={setModalOn}
+            openModal={modalOn}
+            confirmMutation={confirmMutation}
+            setDataGet={setData}
+          />
+        )}
+        {toast.state && (
+          <ToastCustom
+            stateToast={toast.state}
+            body={toast.message}
+            header={toast.header}
+            delay={toast.delay}
+            type={toast.type}
+          />
+        )}
+      </form>
   );
 };
