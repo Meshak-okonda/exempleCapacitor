12c1112d864494c070c8f096e0315810ddfa0259
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-02-26 21:28:01 +0100

    Update total-customers.js

--- src/components/dashboard/total-customers.js
+++ src/components/dashboard/total-customers.js
@@ -1,65 +1,59 @@
 import { Avatar, Box, Card, CardContent, Grid, Typography } from '@mui/material';
 import ArrowUpwardIcon from '@mui/icons-material/ArrowUpward';
 import PeopleIcon from '@mui/icons-material/PeopleOutlined';
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import { useAppSelector } from "../../hooks";
 
-export const TotalCustomers = (props) => (
-  <Card {...props}>
-    <CardContent>
-      <Grid
-        container
-        spacing={3}
-        sx={{ justifyContent: 'space-between' }}
-      >
-        <Grid item>
-          <Typography
-            color="textSecondary"
-            gutterBottom
-            variant="overline"
-          >
-            TOTAL CUSTOMERS
-          </Typography>
-          <Typography
-            color="textPrimary"
-            variant="h4"
-          >
-            1,6k
-          </Typography>
+export const TotalCustomers = (props) => {
+  const { drivers, vehicles } = useAppSelector((state) => state.globalState);
+  const { user } = useAppSelector((state) => state.userConnected);
+
+  return (
+    <Card {...props}>
+      <CardContent>
+        <Grid container spacing={3} sx={{ justifyContent: "space-between" }}>
+          <Grid item>
+            <Typography color="textSecondary" gutterBottom variant="overline">
+              Chauffeurs
+            </Typography>
+            <Typography color="textPrimary" variant="h5">
+              {drivers.filter((dri) => dri.delete != true).length} Chauffeurs
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Avatar
+              sx={{
+                backgroundColor: "success.main",
+                height: 56,
+                width: 56,
+              }}
+            >
+              <PeopleIcon />
+            </Avatar>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Avatar
-            sx={{
-              backgroundColor: 'success.main',
-              height: 56,
-              width: 56
-            }}
-          >
-            <PeopleIcon />
-          </Avatar>
-        </Grid>
-      </Grid>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          pt: 2
-        }}
-      >
-        <ArrowUpwardIcon color="success" />
-        <Typography
-          variant="body2"
+        {user.superAdm && <Box
           sx={{
-            mr: 1
+            alignItems: "center",
+            display: "flex",
+            pt: 2,
           }}
         >
-          16%
-        </Typography>
-        <Typography
-          color="textSecondary"
-          variant="caption"
-        >
-          Since last month
-        </Typography>
-      </Box>
-    </CardContent>
-  </Card>
-);
+          <ArrowDownwardIcon color="error" />
+          <Typography
+            color="error"
+            sx={{
+              mr: 1,
+            }}
+            variant="body2"
+          >
+            {drivers.filter((dri) => dri.delete == true).length}
+          </Typography>
+          <Typography color="textSecondary" variant="caption">
+            Chauffeur(s) supprimer
+          </Typography>
+        </Box>}
+      </CardContent>
+    </Card>
+  );
+}
