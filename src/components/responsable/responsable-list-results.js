dbb44c12c5b6bb2461a214cab994b392515421e2
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-02-27 13:06:51 +0100

    Update responsable-list-results.js

--- src/components/responsable/responsable-list-results.js
+++ src/components/responsable/responsable-list-results.js
@@ -18,43 +18,73 @@ import {
   TextField,
   InputAdornment,
   SvgIcon,
+  Grid,
 } from "@mui/material";
 import { stringAvatar } from "../../utils/getStringAvatar";
 import { useAppDispatch, useAppSelector } from "../../hooks";
 import PopUpSeeResponsable from "./PopUpSeeResponsable";
-import PopUpModifyResponsable from './PopUpModifyResponsable';
+import PopUpModifyResponsable from "./PopUpModifyResponsable";
 import { Search as SearchIcon } from "../../icons/search";
 import ButtonView from "../ButtonView";
 import ButtonDelete from "../ButtonDelete";
 import ButtonEdit from "../ButtonEdit";
-import { DELETE_RESPONSABLE } from '../../graphql/queries';
+import { DELETE_RESPONSABLE, REVERSE_RESPONSABLE } from "../../graphql/queries";
+import { updateResponsableInState } from "../../redux/slice/globalSlice";
 import PopUpMutation from "../custom/PopUpMutation";
-import ToastCustom from '../ToastCustom';
+import ToastCustom from "../ToastCustom";
+import ToggleButton from "@mui/material/ToggleButton";
+import ToggleButtonGroup from "@mui/material/ToggleButtonGroup";
+import ButtonRecovery from "../ButtonRecovery";
 
 export const ResponsableListResults = ({ ...rest }) => {
   const { responsables } = useAppSelector((state) => state.globalState);
   const { user } = useAppSelector((state) => state.userConnected);
+  const dispatch = useAppDispatch();
   const [selectedResponsableIds, setSelectedResponsableIds] = useState([]);
   const [limit, setLimit] = useState(10);
   const [page, setPage] = useState(0);
   const [idSelect, setIdSelect] = useState("");
   const [responsableFilter, setResponsableFilter] = useState([]);
-	const [filter, setFilter] = useState(null);
+  const [filter, setFilter] = useState(null);
   const [responsableView, setResponsableView] = useState(responsables);
   const [modalSeeResponsable, setModalSeeResponsable] = useState(false);
+  const [modalOnRecovery, setModalOnRecovery] = useState(false);
   const [modalUpdateOn, setModalUpdateOn] = useState(false);
   const [modalOnDelete, setModalOnDelete] = useState(false);
+  const [valueView, setValueView] = useState("1");
+  const [data, setData] = useState(null);
   const [toast, setToast] = useState({
-		header: '',
-		body: '',
-		state: false,
-		type: '',
-		delay: 6000,
-	});
+    header: "",
+    body: "",
+    state: false,
+    type: "",
+    delay: 6000,
+  });
 
-  useEffect(()=>{
-    setResponsableView(responsables);
-  }, [responsables])
+  useEffect(() => {
+    if (valueView === "1") {
+      setResponsableView(responsables.filter((dri) => dri.delete === false));
+    } else if (valueView === "2") {
+      setResponsableView(responsables.filter((dri) => dri.delete != false));
+    } else {
+      setResponsableView(responsables);
+    }
+  }, [responsables]);
+
+  useEffect(() => {
+    if (data) {
+      if (data.reverseDeleteResponsable) {
+        setTimeout(() => {
+          dispatch(updateResponsableInState(idSelect));
+        }, 1000);
+        if (idSelect === user.id) {
+          localStorage.removeItem("user");
+          navigate("/");
+        }
+        setValueView("1");
+      }
+    }
+  }, [data, dispatch]);
 
   const handleSelectAll = (event) => {
     let newSelectedResponsableIds;
@@ -68,6 +98,16 @@ export const ResponsableListResults = ({ ...rest }) => {
     }
 
     setSelectedResponsableIds(newSelectedResponsableIds);
+  };
+  const handleChangeView = (event, newValueView) => {
+    if (newValueView === "1") {
+      setResponsableView(responsables.filter((dri) => dri.delete === false));
+    } else if (newValueView === "2") {
+      setResponsableView(responsables.filter((dri) => dri.delete != false));
+    } else {
+      setResponsableView(responsables);
+    }
+    setValueView(newValueView);
   };
 
   const handleSelectOne = (event, id) => {
@@ -105,65 +145,99 @@ export const ResponsableListResults = ({ ...rest }) => {
     setPage(newPage);
   };
   const confirmMutation = async (mutation) => {
-			try {
-				await mutation();
-				setTimeout(() => {
-					dispatch(deleteResponsable(idSelect));
-					navigate('/');
-				}, 1000);
-				localStorage.removeItem('user');
-			} catch (error) {
-				// eslint-disable-next-line no-console
-				setToast({
-					header: 'Erreur',
-					body: `Erreur : ${error.message || ""}`,
-					type: 'error',
-					state: true,
-				});
-			}
-		};
+    try {
+      await mutation();
+      setModalOnDelete(false);
+    } catch (error) {
+      setToast({
+        header: "Erreur",
+        body: `Erreur : ${error.message || ""}`,
+        type: "error",
+        state: true,
+      });
+    }
+  };
+  const confirmMutationRecovery = async (mutation) => {
+    try {
+      await mutation();
+      setModalOnRecovery(false);
+    } catch (error) {
+      setToast({
+        header: "Erreur",
+        body: `Erreur : ${error.message || ""}`,
+        type: "error",
+        state: true,
+      });
+    }
+  };
 
   return (
     <>
       <Box sx={{ mt: 1, mb: 1 }}>
         <Card>
           <CardContent>
-            <Box sx={{ maxWidth: 500 }}>
-              <TextField
-                fullWidth
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <SvgIcon color="action" fontSize="small">
-                        <SearchIcon />
-                      </SvgIcon>
-                    </InputAdornment>
-                  ),
-                }}
-                onChange={({ target: { value } }) => {
-                  setFilter(value);
-                  if (value.trim() === "") {
-                    setResponsableView(responsables);
-                    return 0;
-                  }
-                  setResponsableFilter(
-                    responsables.filter((veh) => {
-                      if (
-                        veh.name.toUpperCase().indexOf(value.toUpperCase()) > -1
-                      )
-                        return veh;
-                    })
-                  );
-                  setResponsableView(
-                    responsableFilter.length < 1
-                      ? responsables
-                      : responsableFilter
-                  );
-                }}
-                placeholder="Search customer"
-                variant="outlined"
-              />
-            </Box>
+            <Grid container spacing={3}>
+              <Grid item md={6} xs={12}>
+                <Box sx={{ maxWidth: 500 }}>
+                  <TextField
+                    fullWidth
+                    InputProps={{
+                      startAdornment: (
+                        <InputAdornment position="start">
+                          <SvgIcon color="action" fontSize="small">
+                            <SearchIcon />
+                          </SvgIcon>
+                        </InputAdornment>
+                      ),
+                    }}
+                    onChange={({ target: { value } }) => {
+                      setFilter(value);
+                      if (value.trim() === "") {
+                        setResponsableView(responsables);
+                        return 0;
+                      }
+                      setResponsableFilter(
+                        responsables.filter((veh) => {
+                          if (
+                            veh.name
+                              .toUpperCase()
+                              .indexOf(value.toUpperCase()) > -1
+                          )
+                            return veh;
+                        })
+                      );
+                      setResponsableView(
+                        responsableFilter.length < 1
+                          ? responsables
+                          : responsableFilter
+                      );
+                    }}
+                    placeholder="Search customer"
+                    variant="outlined"
+                  />
+                </Box>
+              </Grid>
+              {user.superAdm && (
+                <Grid item md={6} xs={12}>
+                  <ToggleButtonGroup
+                    value={valueView}
+                    exclusive
+                    onChange={handleChangeView}
+                    aria-label="text alignment"
+                  >
+                    <ToggleButton value="3" aria-label="left aligned">
+                      Tout
+                    </ToggleButton>
+                    <ToggleButton value="1" aria-label="centered">
+                      Non Supplimer
+                    </ToggleButton>
+                    <ToggleButton value="2" aria-label="right aligned">
+                      Supprimer
+                    </ToggleButton>
+                  </ToggleButtonGroup>
+                </Grid>
+              )}
+            </Grid>
           </CardContent>
         </Card>
       </Box>
@@ -180,7 +254,12 @@ export const ResponsableListResults = ({ ...rest }) => {
                   <TableCell>Numero de telephone</TableCell>
                   <TableCell>Voir</TableCell>
                   {user.upResponsable && <TableCell>Maj</TableCell>}
-                  {user.upResponsable && <TableCell>Sup</TableCell>}
+                  {user.upResponsable && valueView == "1" && (
+                    <TableCell>Sup</TableCell>
+                  )}
+                  {user.delVehicle && valueView == "2" && (
+                    <TableCell>Restorer</TableCell>
+                  )}
                 </TableRow>
               </TableHead>
               <TableBody>
@@ -237,12 +316,25 @@ export const ResponsableListResults = ({ ...rest }) => {
                         />
                       </TableCell>
                     )}
-                    {user.delResponsable && (
+                    {user.delResponsable &&
+                      !responsable.delete &&
+                      valueView == "1" &&
+                      !responsable.superAdm && (
+                        <TableCell>
+                          <ButtonDelete
+                            onClick={() => {
+                              setIdSelect(responsable.id);
+                              setModalOnDelete(true);
+                            }}
+                          />
+                        </TableCell>
+                      )}
+                    {user.delVehicle && responsable.delete && valueView == "2" && (
                       <TableCell>
-                        <ButtonDelete
+                        <ButtonRecovery
                           onClick={() => {
+                            setModalOnRecovery(true);
                             setIdSelect(responsable.id);
-                            setModalOnDelete(true);
                           }}
                         />
                       </TableCell>
@@ -282,6 +374,15 @@ export const ResponsableListResults = ({ ...rest }) => {
             query={DELETE_RESPONSABLE(idSelect)}
             setModalON={setModalOnDelete}
             confirmMutation={confirmMutation}
+          />
+        )}
+        {modalOnRecovery && (
+          <PopUpMutation
+            openModal={modalOnRecovery}
+            query={REVERSE_RESPONSABLE(idSelect)}
+            setDataGet={setData}
+            setModalON={setModalOnRecovery}
+            confirmMutation={confirmMutationRecovery}
           />
         )}
         {toast.state && (
