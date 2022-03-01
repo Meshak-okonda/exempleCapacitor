d4d87f3900a851a5ab7b060537d9103c15eb5617
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-03-01 23:45:02 +0100

    Delete ButtonEdit.js

--- components/custom/ButtonEdit.js
+++
@@ -1,9 +0,0 @@
-import { Button } from 'primereact/button';
-
-export default function ButtonEdit(props) {
-	return (
-		<Button className='p-button-info' {...props}>
-			<i className='bi bi-pencil-square'></i>
-		</Button>
-	);
-}
