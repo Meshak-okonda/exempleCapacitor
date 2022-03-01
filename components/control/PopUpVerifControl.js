dbac3f206992bedbe60b3a2d3c6967d219f47f24
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-03-01 23:44:57 +0100

    Update PopUpVerifControl.js

--- components/control/PopUpVerifControl.js
+++ components/control/PopUpVerifControl.js
@@ -1,13 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { Modal, Spinner } from 'react-bootstrap';
+import React from "react";
+import { Spinner } from "react-bootstrap";
 import { GetFrenchElementControl } from "../../utils";
-import PopOver from '../custom/PopOver';
-import ButtonYellow from '../custom/ButtonYellow';
-import { useMutation } from '@apollo/client';
-import { CREATE_CONTROL_VEHICLE } from '../../graphql/queries';
-import ToastCustom from "../ToastCustom";
+import PopOver from "../custom/PopOver";
+import { useMutation } from "@apollo/client";
+import { CREATE_CONTROL_VEHICLE } from "../../graphql/queries";
+import ToastCustom from "../custom/ToastCustom";
 import PropTypes from "prop-types";
-import Button from "@mui/material/Button";
 import { styled } from "@mui/material/styles";
 import Dialog from "@mui/material/Dialog";
 import DialogTitle from "@mui/material/DialogTitle";
@@ -15,118 +13,111 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogActions from "@mui/material/DialogActions";
 import IconButton from "@mui/material/IconButton";
 import CloseIcon from "@mui/icons-material/Close";
-import Typography from "@mui/material/Typography";
-import Select from "@mui/material/Select";
-import TextField from "@mui/material/TextField";
-import MenuItem from "@mui/material/MenuItem";
-import InputLabel from "@mui/material/InputLabel";
 import ButtonSubmit from "../ButtonSubmit";
-import {
-  Grid,
-} from "@mui/material";
+import { Grid } from "@mui/material";
 
 export default function PopUpVerifControl({
-	modalOn,
-	setModalOn,
-	dataControl,
-	date,
-	idVehicle,
-	reset,
+  modalOn,
+  setModalOn,
+  dataControl,
+  date,
+  idVehicle,
+  reset,
+  setDay,
 }) {
-	const [mutation, { data, loading, error }] = useMutation(
-		CREATE_CONTROL_VEHICLE
-	);
-	const handleClose = () => setModalOn(false);
-	const styleResum = { width: 80, textAlign: 'center', borderRadius: 5 };
-	let stateVehicle = {
-		damaged: 0,
-		good: 0,
-		missing: 0,
-	};
-	dataControl.map((data) => {
-		if (data[data.name] && data[data.name].state) {
-			switch (data[data.name].state) {
-				case 'Bonne':
-					stateVehicle.good += 1;
-					break;
-				case 'Abimé':
-					stateVehicle.damaged += 1;
-					break;
-				case 'Manque':
-					stateVehicle.missing += 1;
-					break;
-				default:
-					break;
-			}
-		}
-	});
-	if (data) {
-		reset();
-		setTimeout(() => {
-			handleClose();
-		}, 2000)
-		return (
-			<ToastCustom
-				stateToast={true}
-				body='Controle ajouté avec succès'
-				header='Felicitation'
-				type='success'
-				delay={5000}
-			/>
-		);
-	}
-	if (error) {
-		return (
-			<ToastCustom
-				stateToast={true}
-				body="Erreur lors de l\'ajout du controle"
-				header='Erreur'
-				type='danger'
-				delay={5000}
-			/>
-		);
-	}
+  const [mutation, { data, loading, error }] = useMutation(
+    CREATE_CONTROL_VEHICLE
+  );
+  const handleClose = () => setModalOn(false);
+  let stateVehicle = {
+    damaged: 0,
+    good: 0,
+    missing: 0,
+  };
+  dataControl.map((data) => {
+    if (data[data.name] && data[data.name].state) {
+      switch (data[data.name].state) {
+        case "Bonne":
+          stateVehicle.good += 1;
+          break;
+        case "Abimé":
+          stateVehicle.damaged += 1;
+          break;
+        case "Manque":
+          stateVehicle.missing += 1;
+          break;
+        default:
+          break;
+      }
+    }
+  });
+  if (data) {
+    reset();
+    handleClose();
+    setDay(null);
+    return (
+      <ToastCustom
+        stateToast={true}
+        body="Controle ajouté avec succès"
+        header="Felicitation"
+        type="success"
+        delay={5000}
+      />
+    );
+  }
+  if (error) {
+    return (
+      <ToastCustom
+        stateToast={true}
+        body="Erreur lors de l\'ajout du controle"
+        header="Erreur"
+        type="danger"
+        delay={5000}
+      />
+    );
+  }
 
-	const validateControl = () => {
-		let dataFormat = {};
-		dataControl.forEach((data) => {
-			if (data[data.name] && data[data.name].state) {
-				dataFormat[data.name] = {
-					state: data[data.name].state,
-				};
-				data[data.name].comment
-					? (dataFormat[data.name].comment = data[data.name].comment)
-					: null;
-				data[data.name].image
-					? (dataFormat[data.name].image = data[data.name].image)
-					: null;
-			}
-			if (data.name == 'mileage') {
-				dataFormat[data.name] = data[data.name];
-			}
-		});
-		dataFormat = {
-			...dataFormat,
-			stateVehicle,
-			dateVerification: date,
-			idVehicle,
-		};
-		try {
-			mutation({
-				variables: {
-					vehicleVerification: dataFormat,
-				},
-			});
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	const formatBody = (name, body) => {
-		if (name === mileage) {
-			return body;
-		}
-	};
-	return (
+  const validateControl = () => {
+    let dataFormat = {};
+    dataControl.forEach((data) => {
+      if (data[data.name] && data[data.name].state) {
+        dataFormat[data.name] = {
+          state: data[data.name].state,
+        };
+        data[data.name].comment
+          ? (dataFormat[data.name].comment = data[data.name].comment)
+          : null;
+        data[data.name].image
+          ? (dataFormat[data.name].image = data[data.name].image)
+          : null;
+      }
+      if (data.name == "mileage") {
+        dataFormat[data.name] = data[data.name];
+      }
+    });
+    dataFormat = {
+      ...dataFormat,
+      stateVehicle,
+      dateVerification: date,
+      idVehicle,
+    };
+    try {
+      mutation({
+        variables: {
+          vehicleVerification: dataFormat,
+        },
+      });
+    } catch (error) {
+      // eslint-disable-next-line no-console
+      console.log(error);
+    }
+  };
+  const formatBody = (name, body) => {
+    if (name === mileage) {
+      return body;
+    }
+  };
+  return (
     <>
       <BootstrapDialog
         onClose={handleClose}
@@ -150,28 +141,30 @@ export default function PopUpVerifControl({
               display: "flex",
               justifyContent: "space-evenly",
             }}
-            spacing={3}
           >
             <Grid
               item
-              style={styleResum}
-              className="bg-success d-flex justify-content-center align-items-center bold"
+              className="bold text-center"
+              sx={{ p: 0.7, borderRadius: 1 }}
+              style={{ backgroundColor: "#003863", color: "white" }}
             >
               Bonne <br />
               {stateVehicle.good === 0 ? "Aucun" : stateVehicle.good}
             </Grid>
             <Grid
               item
-              style={styleResum}
-              className="bg-warning d-flex justify-content-center align-items-center bold"
+              sx={{ p: 0.7, borderRadius: 1 }}
+              style={{ backgroundColor: "#e53935", color: "white" }}
+              className="bold text-center"
             >
               Abimé <br />
               {stateVehicle.damaged === 0 ? "Aucun" : stateVehicle.damaged}
             </Grid>
             <Grid
               item
-              style={styleResum}
-              className="bg-danger d-flex justify-content-center align-items-center p-0 center bold"
+              sx={{ p: 0.7, borderRadius: 1 }}
+              style={{ backgroundColor: "#FB8C00", color: "white" }}
+              className="bold text-center"
             >
               Manque <br />
               {stateVehicle.missing === 0 ? "Aucun" : stateVehicle.missing}
@@ -209,7 +202,6 @@ export default function PopUpVerifControl({
     </>
   );
 }
-
 
 const BootstrapDialog = styled(Dialog)(({ theme }) => ({
   "& .MuiDialogContent-root": {
