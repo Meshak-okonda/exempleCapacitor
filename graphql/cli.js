d2e5258374880ab6b90d419b4ce6fe2d040e0e9b
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-03-08 14:38:19 +0100

    feat: add readme file

--- graphql/cli.js
+++ graphql/cli.js
@@ -1,53 +1,14 @@
 import {
   ApolloClient,
   InMemoryCache,
-  ApolloLink,
-  concat,
-  split,
-  HttpLink,
+  ApolloProvider,
+  useQuery,
+  gql,
 } from "@apollo/client";
-import { getMainDefinition } from "@apollo/client/utilities";
-import { WebSocketLink } from "@apollo/client/link/ws";
-
-const httpLink = new HttpLink({
-  uri: "https://cpsfleets.herokuapp.com/graphql",
-});
-// const wsLink = new WebSocketLink({
-// 	uri: 'ws://cpsfleets.herokuapp.com/graphql',
-// 	timeout: 300000,
-// 	options: {
-// 		reconnect: true,
-// 	},
-// });
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === "OperationDefinition" ||
-      definition.operation === "subscription"
-    );
-  },
-  // wsLink,
-  httpLink
-);
-
-const authMiddleware = new ApolloLink((operation, forward) => {
-  // add the authorization to the headers
-  operation.setContext(({ headers = {} }) => ({
-    headers: {
-      ...headers,
-      // authorization: localStorage.getItem("token") || null,
-    },
-  }));
-
-  return forward(operation);
-});
 
 const client = new ApolloClient({
-  link: concat(authMiddleware, splitLink),
-  cache: new InMemoryCache({
-    addTypename: false,
-  }),
+  uri: process.env.URL_GRAPHQL,
+  cache: new InMemoryCache({ addTypename: false }),
 });
 
 export default client;
