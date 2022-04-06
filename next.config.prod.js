b5f2035bfd17a6354d13dadfcad7034299b679bb
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-04-06 10:35:38 +0100

    Update next.config.prod.js

--- next.config.prod.js
+++ next.config.prod.js
@@ -9,7 +9,7 @@ module.exports = withPWA({
     runtimeCaching,
   },
   env: {
-    URL_GRAPHQL: "http://35.176.0.228:6000/graphql",
+    URL_GRAPHQL: "https://fleet.congoparkingservice.com/graphql",
     URL_ClOUDINARY: "https://api.cloudinary.com/v1_1/drngt0gv3/image/upload",
   },
 });
