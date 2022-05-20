9c3b8bda23dd8177fe6ee20ef7935aaff73dc6bc
Meshak Ahoka <59387295+Meshak-okonda@users.noreply.github.com>
2022-05-20 16:31:15 +0100

    d

--- next.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  env: {
-    URL_GRAPHQL: "https://fleet.congoparkingservice.com/graphql",
-    URL_ClOUDINARY: "https://api.cloudinary.com/v1_1/drngt0gv3/image/upload",
-  },
-};
